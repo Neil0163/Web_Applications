b3300f05c815e7a567948463c918eeebc65f58ec
Neil Uddin <neil.uddin@hotmail.com>
2023-12-06 10:03:01 +0000

    latest commit

--- app.py
+++ app.py
@@ -5,11 +5,42 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # == Your Routes Here ==
+#Phase 1
+#Create app.route(path, methods)
+#Def function name
+#Create exsisting name list 
+# print statement for exsiting list
+# Create add_name var with request for arg.get(argument)
+#add print statement f string names to be added from add_name
+#Phase 2
+#create if statement for add_name
+#nested within add name create new_name var set to add_name and split at comma
+#print to check new name added
+#Phase 3
+#create a for loop to iterrate over name in new name
+#append to esisting name and strip name
+#print final list of names from esisting names using F string 
+# return the result using ','.join to esiting names
+#run the app 
+#check the broswer at http://127.0.0.1:5001/names?add=Eddie
+#check the terminal for print statements 
+@app.route('/names', methods = ['GET'])
+def get_add_name():
+    existing_name = ['Julia', 'Alice', 'Karim']
+    print(f" this is the existing list {existing_name}")
+    add_name = request.args.get('add')
+    print(f"this is the name that will be added to the list {add_name}")
+    if add_name:
+        new_name = add_name.split(',')
+        print(f'checks name is returned as list {new_name}')
+        for name in new_name:
+            existing_name.append(name.strip())
+            print(f"checks that eddie has been added to list {existing_name}")
+    updated_name = existing_name
+    print(f"This is the final list {updated_name}")
+    return','.join(updated_name)
 
-@app.route('/wave', methods= ['GET'])
-def post_submit():
-    name = request.args['name']
-    return f"I am waving at {name}"
+
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
 # if started in test mode.
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,2 +1,6 @@
 # Tests for your routes go here
 
+def test_for_get_add_name(web_client):
+    response = web_client.get('/names?add=Eddie')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Julia,Alice,Karim,Eddie'
